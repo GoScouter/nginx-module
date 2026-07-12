1a508235191e1a15a8d13f90b364eff4d3ff24fa
Idan Koblik <Idan Koblik>
2026-07-12 19:37:46 +0300

    Add main entry

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 // POC
 
-package module
+package main
 
-import "github.com/GoScouter/sdk"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/GoScouter/sdk"
+)
 
 type NginxModule struct {}
 
@@ -28,3 +35,41 @@ func (r *NginxResult) Render() string {
     return "TODO"
 }
 
+func main() {
+	module := &NginxModule{}
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "expected a subcommand: describe or scout")
+		os.Exit(2)
+	}
+
+	switch os.Args[1] {
+	case "describe":
+		enc := json.NewEncoder(os.Stdout)
+		if err := enc.Encode(sdk.Descriptor{
+			Protocol:    sdk.ProtocolVersion,
+			Name:        module.Name(),
+			Description: module.Description(),
+			Version:     module.Version(),
+		}); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case "scout":
+		fs := flag.NewFlagSet("scout", flag.ExitOnError)
+		target := fs.String("target", "", "target to scout")
+		_ = fs.Parse(os.Args[2:])
+
+		result, err := module.Scout(*target)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Print(result.Render())
+
+	default:
+		fmt.Fprintf(os.Stderr, "unknown subcommand %q\n", os.Args[1])
+		os.Exit(2)
+	}
+}
