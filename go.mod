310c9d629fe96ffe077ea8d8466e6f2321fdb678
Idan Koblik <Idan Koblik>
2026-07-15 12:52:55 +0300

    Update nginx module

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module nginx-module
 
 go 1.26.4
 
-require github.com/GoScouter/sdk v0.0.0-20260712154204-fa4dc6e57c6f // indirect
+require github.com/GoScouter/sdk v0.0.0-20260712154204-fa4dc6e57c6f
+
+replace github.com/GoScouter/sdk => ../sdk
--- main.go
+++ main.go
@@ -3,73 +3,39 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/GoScouter/sdk"
 )
 
-type NginxModule struct {}
+type NginxModule struct{}
 
 func (module *NginxModule) Name() string {
-    return "Nginx"
+	return "Nginx"
 }
 
 func (module *NginxModule) Description() string {
-    return "Checks site information regarding Nginx"
+	return "Checks site information regarding Nginx"
 }
 
 func (module *NginxModule) Version() string {
-    return "0.0.1"
+	return "0.0.1"
 }
 
-func (module *NginxModule) Scout(target string) (sdk.Result, error) {
-    return &NginxResult{}, nil
+func (module *NginxModule) Scout(target string, args []string) (sdk.Result, error) {
+	return &NginxResult{}, nil
 }
 
-type NginxResult struct {}
+type NginxResult struct{}
 
 func (r *NginxResult) Render() string {
-    return "TODO"
+	return "TODO\r\n"
 }
 
 func main() {
-	module := &NginxModule{}
-
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "expected a subcommand: describe or scout")
-		os.Exit(2)
-	}
-
-	switch os.Args[1] {
-	case "describe":
-		enc := json.NewEncoder(os.Stdout)
-		if err := enc.Encode(sdk.Descriptor{
-			Protocol:    sdk.ProtocolVersion,
-			Name:        module.Name(),
-			Description: module.Description(),
-			Version:     module.Version(),
-		}); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-	case "scout":
-		fs := flag.NewFlagSet("scout", flag.ExitOnError)
-		target := fs.String("target", "", "target to scout")
-		_ = fs.Parse(os.Args[2:])
-
-		result, err := module.Scout(*target)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		fmt.Print(result.Render())
-
-	default:
-		fmt.Fprintf(os.Stderr, "unknown subcommand %q\n", os.Args[1])
-		os.Exit(2)
+	if err := sdk.Serve(&NginxModule{}); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
